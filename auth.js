01856058cc2751b33461fb1c270e00f37b084174
tatianaisabel <62834339+tatianaisabel@users.noreply.github.com>
2024-12-14 11:14:47 -0500

    Update auth.js so that it throws errors

--- auth.js
+++ auth.js
@@ -5,19 +5,40 @@ const https = require('https');
 const passwordHash = async (password) => {
   const sbcrypt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, sbcrypt);
+  try {
+    const scrypt = await bcrypt.genSalt(SALT_ROUNDS);
+    return await bcrypt.hash(password, scrypt);
+  } catch (err) {
+    throw new Error('There was an error when hashing password: ' + err.message);
+  }
 };
 
 const passwordCheck = async (password, hashP) => {
-  return await bcrypt.compare(password, hashP);
+  if (!password || !hashP) {
+    throw new Error('Please given password and hash);
+  }  
+  try {
+    return await bcrypt.compare(password, hashP);
+  } catch (err) {
+    throw new Error('There was an error finding the password: ' + err.message);
+  }
 };
 
 const sslOptions = {
-  key: fs.readFileSync('private-key.pem'),
-  cert: fs.readFileSync('certificate.pem')
+  try {
+    return {
+      key: fs.readFileSync('private-key.pem'),
+      cert: fs.readFileSync('certificate.pem')
+    };
+  } catch (err) {
+    console.error('Had trouble loading SSL certificates:', err.message);
+    console.error('Do the private-key.pem and certificate.pem exist in the root?');
+    process.exit(1);
+  }
 };
 
 module.exports = {
   passwordHash,
   passwordCheck,
-  sslOptions
+  sslOptions: loadSSLCertificates()
 };
